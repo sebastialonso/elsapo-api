b688e871546ea8edddecda5f7bf69e402489f550
Sebastian Gonzalez <sebagonz91@gmail.com>
2014-05-11 20:27:24 -0400

    Verificacion de CORS para busesController

--- app/controllers/buses_controller.rb
+++ app/controllers/buses_controller.rb
@@ -1,5 +1,16 @@
 class BusesController < ApplicationController
+  skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
   respond_to :json
+  after_filter :set_access_control_headers
+
+  # def options
+  #   if access_allowed?
+  #     set_access_control_headers
+  #     head :ok
+  #   else
+  #     head :forbidden
+  #   end
+  # end
 
   def index
     @buses = Bus.all
@@ -25,4 +36,14 @@ class BusesController < ApplicationController
     @sapeada = Sapeada.where(:bus_id => params[:bus_id]).last
   end
 
+  private
+    # def set_access_control_headers
+    #   headers['Access-Control-Allow-Origin'] = "*"
+    #   headers['Access-Control-Request-Methods'] = 'POST, GET, OPTIONS'
+    # end
+
+    # def access_allowed?
+    #   allowed_sites = [request.env['HTTP_ORIGIN']]
+    #   return allowed_sites.include?(request.env['HTTP_ORIGIN'])
+    # end
 end
